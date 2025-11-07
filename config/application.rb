d9053b9907e3d60e618ad4b483c6d6d7dcecc83f
CTimmins1 <conorbartends@gmail.com>
2025-11-07 13:17:39 +0000

    tests: use RSpec-only generators; remove Minitest and simplify CI

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,13 @@ module PimsApp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.system_tests nil   # don’t generate Rails/Minitest system tests
+      
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
