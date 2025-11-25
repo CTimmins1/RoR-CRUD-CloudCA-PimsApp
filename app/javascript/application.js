3ad2deaf94f29c73b16b1f97caf897b2dbc63e7f
CTimmins1 <conorbartends@gmail.com>
2025-11-25 09:40:12 +0000

    Remove old frontend

--- app/javascript/application.js
+++
@@ -1,5 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-//Failed to import this here causing an error when trying to load my charts
-import "chartkick"
