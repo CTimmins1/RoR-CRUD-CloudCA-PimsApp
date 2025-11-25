2c5eb86aa8d0c0115c1539fb0ceb9dfa02428957
CTimmins1 <conorbartends@gmail.com>
2025-11-25 09:43:19 +0000

    Clean main branch and add project overview

--- spec/factories/projects.rb
+++
@@ -1,8 +0,0 @@
-# spec/factories/projects.rb
-FactoryBot.define do
-  factory :project do
-    title { "Test Project #{SecureRandom.hex(4)}" }
-    description { "A sample description for a new project." }
-    status { :pending }
-  end
-end
