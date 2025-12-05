e3f3bfd23c425006db0a14be1578cec89dec760b
CTimmins1 <conorbartends@gmail.com>
2025-12-05 10:31:17 +0000

    feat: final backend - JWT auth + seeded user only + multi-tenant projects/tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   namespace :api do
     namespace :v1 do
-      post "/signup", to: "registrations#create"
-      post "/login",  to: "auth#create"
+      post "/login", to: "auth#create"
 
       resources :projects do
         resources :tasks
