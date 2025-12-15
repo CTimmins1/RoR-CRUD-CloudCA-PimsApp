7b5848606039826a081d24567961ce3aa73f685d
CTimmins1 <conorbartends@gmail.com>
2025-12-15 20:01:14 +0000

    Fixed task editing, no longer all fails

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-     post "login", to: "sessions#create"	
+      post "login", to: "sessions#create"
+
       resources :projects do
-        resources :tasks
+        resources :tasks, only: [:index, :create]
       end
+
+      # added these to enable to me to edit tasks
+      resources :tasks, only: [:update, :destroy]
     end
   end
 end
+
